dbc154ab43f51d5003ec2483162c7acdba5a47db
squirrel_icdi <squirrel.study@gmail.com>
2013-09-20 21:23:48 +0300

    first_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4fa813e410e5036b82bef8ca70a964523ff326b0927da844c5077d7f246a1425607042a0bed8a20bc32551e32f69f511b9f181e97e342924632806b5788609ea'
